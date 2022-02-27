ca6a61da5d11a3e036cbaa955b25cc431894d364
NanditaBagga <nanditabagga23@gmail.com>
2022-02-28 01:01:41 +0530

    fixed pypy deployment

--- setup.py
+++ setup.py
@@ -5,19 +5,25 @@ with open("README.md", "r") as fh:
 
 setup(
 	name="TOPSIS-NANDITA-101917191",
-	version='1.0',
+	version='2.0',
 	author='Nandita',
 	author_email='nanditabagga23@gmail.com',
 	description='topsis package for MCDM problems',
 	long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NanditaBagga/TOPSIS-NANDITA-101917191",
+    download_url="https://github.com/NanditaBagga/TOPSIS-NANDITA-101917191/archive/refs/tags/2.0.tar.gz",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'topsis=topsis.topsis:main'
+            'topsis=topsis.101917191:main'
         ]
     },
+    install_requires=[
+        'numpy',
+        'pandas',
+        'argparse',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -26,4 +32,4 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-	)
+)
